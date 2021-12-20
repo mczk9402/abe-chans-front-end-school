fea8fb6ea1d709f0553561273e7e18a221c37cb3
望月祐汰 <mczk9402@gmail.com>
2021-12-21 01:12:17 +0900

    🎉 WebAPIを用いた画像検索アプリ

---
+++ pixabay/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+import logo from './logo.png';
+
+import './styles/imagesList.css';
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const ApiKey = process.env.REACT_APP_PIXABAY_APIKEY;
+
+  const onSubmit = async (term) => {
+    try {
+      const params = {
+        key: ApiKey,
+        q: term,
+      };
+
+      const response = await axios.get('https://pixabay.com/api/', { params });
+      setImages(response.data.hits);
+      if (response.data.total === 0) {
+        alert('お探しの画像はありません');
+      }
+    } catch {
+      alert('画像の取得に失敗しました');
+    }
+    console.log(term);
+  };
+
+  return (
+    <div className="ui container" style={{ paddingTop: '20px' }}>
+      <img src={logo} alt="pixabay-logo" className="pixabay-logo" />
+      <SearchBar onSubmit={onSubmit} />
+      <ImageList images={images} />
+    </div>
+  );
+};
+
+export default App;
